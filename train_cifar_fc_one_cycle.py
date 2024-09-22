0a91bf4265cd22e1aa21670b14fb37d7a7414388
maherhanut <maher.hanut@gmail.com>
2024-09-22 18:02:11 +0300

    s

--- train_cifar_fc_one_cycle.py
+++ train_cifar_fc_one_cycle.py
@@ -212,7 +212,7 @@ def train_PFA_cifar10_exp_decay(session_name, layer, max_lr =1e-4, bn=512, ranks
                             ExponentialLR,
                             rf"/home/maherhanut/Documents/projects/EarlyVisualRepresentation_pfa/artifacts/{dset_name}/{session_name}/{layer}/r_{rank}/exp_{i}",
                             optimizer_params={'lr': max_lr, 'weight_decay': decay, 'amsgrad': True},
-                            scheduler_params={'gamma': 0.98},  #0.97
+                            scheduler_params={'gamma': 0.975},  #0.97
                             device=device
                             )
             
@@ -349,7 +349,7 @@ if __name__ == "__main__":
     
     
     # train_PFA_cifar10_exp_decay('512x4_no_drop_out_V2', 'layer2', max_lr= 4e-4, bn=512, ranks=[1, 2, 3, 4, 5, 6, 8, 10, 16, 32], decay=6e-4, update_p = True)
-    train_PFA_cifar10_exp_decay('test_4e4_6e-4', 'layer3', max_lr= 5e-4, bn=512, ranks=[1, 2, 3, 4, 5, 6, 8, 10, 16, 32][::-1], decay=7e-4, update_p = True)
+    # train_PFA_cifar10_exp_decay('test_4e4_6e-4', 'layer3', max_lr= 5e-4, bn=512, ranks=[1, 2, 3, 4, 5, 6, 8, 10, 16, 32][::-1], decay=7e-4, update_p = True)
     # train_PFA_cifar10_exp_decay('512x4_no_drop_out_V2', 'layer4', max_lr= 4e-4, bn=512, ranks=[1, 2, 3, 4, 5, 6, 8, 10], decay=6e-4, update_p = True)
     
     # train_PFA_cifar10_constraint_all('512x4_no_drop_out_V2', rank=[16, 8, 4, 2, 1], max_lr=4e-4, bn=512, decay=5e-4)
@@ -360,3 +360,8 @@ if __name__ == "__main__":
     # train_PFA_cifar100_subsets('v1', layer='layer4', max_lr=4e-4, bn=512, ranks=[1, 2, 4, 8, 16, 32, 40], decay=5e-5, class_limit=40)
     # train_PFA_cifar100_subsets('v1', layer='layer2', max_lr=4e-4, bn=512, ranks=[1, 2, 4, 8, 16, 32, 64, 100], decay=5e-5, class_limit=100)
     # train_PFA_cifar100_subsets_BP('v1', max_lr=4e-4, bn=512, decay=5e-5, class_limit=40)
+    
+    
+    
+    
+    train_PFA_cifar10_exp_decay('test_4e4_6e-4', 'layer2', max_lr= 6e-4, bn=512, ranks=[1, 2, 3, 4, 5, 6, 8, 10, 16, 32][::-1], decay=7e-4, update_p = True)
